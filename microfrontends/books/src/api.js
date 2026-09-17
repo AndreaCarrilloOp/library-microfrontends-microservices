03eb98ae1a7730280936dd4436e49660d085fbf8
Andrea Carrillo <acarrillo.vlk@gmail.com>
2026-09-16 21:40:39 -0600

    update: configure production microfrontend URLs

--- microfrontends/books/src/api.js
+++ microfrontends/books/src/api.js
@@ -2,6 +2,8 @@
 export const booksApi = 'https://book-microservice.netlify.app'
 
 // Mini-sitios publicados. Reemplace estas URLs al desplegar.
-export const authorsMinisite = 'https://author-microfrontend.netlify.app'
+export const authorsMinisite =
+  'https://AndreaCarrilloOp.github.io/library-microfrontends-microservices/microfrontends/authors'
 export const publishersMinisite = 'https://publisher-microfrontend.vercel.app'
-export const reviewsMinisite = 'https://REPLACE_REVIEWS_MINISITE_URL'
+export const reviewsMinisite =
+  'https://AndreaCarrilloOp.github.io/library-microfrontends-microservices/microfrontends/reviews'
